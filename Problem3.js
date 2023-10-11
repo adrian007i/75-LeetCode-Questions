5a1f65bf1b3880add0726a30f94e6b9149dee8de
Adrian John <adrianjohn.developer@gmail.com>
2023-10-11 02:25:06 -0500

    Problem 3

---
+++ Problem3.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/contains-duplicate/
+// input : nums : array
+// output : boolean
+
+
+var containsDuplicate = function(nums) {
+    
+    let map = {};
+    
+    for(let i=0; i<nums.length; i++){
+        if(!map[nums[i]]){
+            map[nums[i]] = true;
+        }else{
+            return true;
+        }
+    }
+    return false
+    
+};
